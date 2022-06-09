4317672fa5d9f246939df7c31a708b042b22bbce
Carol Yan <dreamfx@hotmail.com>
2022-06-09 12:31:03 -0700

    fix typo error

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { StaleWhileRevalidate, CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -30,7 +30,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 registerRoute(
   // Define the callback function that filter the requests we want to cache
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StableWhileRevalidate({
+  new StaleWhileRevalidate({
     // Name of the cache storage
     cacheName: 'asset-cache',
     // Plugin that cache responses to a maximum-age of 30 days
